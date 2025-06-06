81c238699805a24c67eeb92aae44bc0f18857a25
Yami <yamilaveralopez@gmail.com>
2025-06-05 23:23:13 -0300

    fix errors build

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic__RGwoPeZ.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/_functions/pages/home.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/Home_BZ853DRf.mjs';
-export { renderers } from '../renderers.mjs';
-
-const page = () => _page;
-
-export { page };
